1da48fdd4dfe1a673786165081a5acb28c09c3c4
CasasNoelia <61793967+CasasNoelia@users.noreply.github.com>
2020-03-27 01:25:23 -0300

    login + register page

---
+++ Ingreso/micuenta/urls.py
@@ -0,0 +1,14 @@
+from django.urls import path
+from . import views
+
+urlpatterns = [
+
+    path('register/', views.registerPage, name="register"),
+    path('login/', views.loginPage, name="login"),
+    path('logout/', views.logoutUser, name="logout"),
+
+    path('', views.Home, name="Home"),
+    path('usuario_registro/', views.Usuario_Registro),
+    path('empleo_registro/', views.Empleo_Registro),
+    path('empresa_registro/', views.Empresa_Registro)
+]
---
+++ Ingreso/micuenta/views.py
@@ -0,0 +1,62 @@
+from django.shortcuts import render,redirect
+from django.http import HttpResponse
+from django.contrib.auth.forms import UserCreationForm
+
+from django.contrib.auth import authenticate, login, logout
+
+from django.contrib import messages
+
+
+def registerPage(request):
+    form = CreateUserForm()
+
+    if request.method == 'POST':
+        form = CreateUserForm(request.POST)
+        if form.is_valid():
+            form.save()
+            user = form.cleaned_data.get('username')
+            messages.success(request, user + ' tu cuenta fue creada con exito')
+            return redirect('login')
+
+    context = {'form': form}
+    return render(request, 'micuenta/register.html', context)
+
+def loginPage(request):
+
+    if request.method == 'POST':
+        username = request.POST.get('username')
+        password = request.POST.get('password')
+
+        user = authenticate(request, username=username, password=password)
+
+        if user is not None:
+            login(request, user)
+            return redirect('Home')
+        else:
+            messages.info(request, 'su usuario o contraseña son incorrectos')
+
+    context = {}
+    return render(request, 'micuenta/login.html', context)
+
+def logoutUser(request):
+    logout(request)
+    return redirect('login')
+
+def Home(request):
+    return render(request, 'micuenta/dashboard.html')
+
+
+def Recibo(request):
+    return render(request, 'micuenta/recibo.html')
+
+
+def Usuario_Registro(request):
+    return render(request, 'usuario_registro.html')
+
+
+def Empleo_Registro(request):
+    return render(request, 'empleo_registro.html')
+
+
+def Empresa_Registro(request):
+    return render(request, 'empresa_registro.html')
